a76cf8cdb91533ff8420ad8ef5f8d977d28337b1
Andy Du-frigstad <47784086+s278552@users.noreply.github.com>
2019-04-10 19:45:06 -0400

    made the Logo fade in

--- CompanyAnimationAndyDF/main.lua
+++ CompanyAnimationAndyDF/main.lua
@@ -6,6 +6,9 @@
 -- Hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
+--vsetting the scroll Speed
+scrollSpeed = 3
+
 -- local Variables
 local IntroSound = audio.loadSound( "Sounds/bensound-dubstep.mp3" )
 local IntroSoundChannel
@@ -20,4 +23,18 @@ display.setDefault ("background", 1, 0, 0)
 transition.to(CompanyImage, {rotation = CompanyImage.rotation - 360, time = 1000, onComplete = spinImage})
 transition.to(CompanyImage, {x = 512, y = 384, time = 4000})
 
+-- making the Logo be transparent
+CompanyImage.alpha = 0
+
+-- playing the sound
 IntroSoundChannel = audio.play(IntroSound)
+
+local function MoveLogo(event)
+	--add the scroll speed to the x-value of the logo
+	CompanyImage.x = CompanyImage.x + scrollSpeed
+	--change the transparency of the logo every time it moves so that it fades out
+	CompanyImage.alpha = CompanyImage.alpha + 0.01
+end
+
+--moveShip will de called over and over again
+Runtime:addEventListener("enterFrame", MoveLogo)
