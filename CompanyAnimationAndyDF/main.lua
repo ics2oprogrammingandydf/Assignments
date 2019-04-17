eb8091914a77f8bb47fbe6bec83082e900470697
Andy Du-frigstad <47784086+s278552@users.noreply.github.com>
2019-04-17 09:04:47 -0400

    updated the Image for my compnay logo

--- CompanyAnimationAndyDF/main.lua
+++ CompanyAnimationAndyDF/main.lua
@@ -6,18 +6,35 @@
 -- Hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
---vsetting the scroll Speed
-scrollSpeed = 3
+-----------------------------------------------------------------------------------------
+-- SOUND
+-----------------------------------------------------------------------------------------
 
--- local Variables
 local IntroSound = audio.loadSound( "Sounds/bensound-dubstep.mp3" )
 local IntroSoundChannel
+IntroSoundChannel = audio.play(IntroSound)
 
-local CompanyImage = display.newImage( "Images/IMG_4635 (2).png" )
-CompanyImage:scale (0.2, 0.2)
+-----------------------------------------------------------------------------------------
+-- GLOBAL VARIABLES
+-----------------------------------------------------------------------------------------
 
+--setting the scroll Speed
+scrollSpeed = 50
+
+-----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+local CompanyImage
+
+-----------------------------------------------------------------------------------------
+-- OBJECT CREATION
+-----------------------------------------------------------------------------------------
 -- setting the background
 display.setDefault ("background", 1, 0, 0)
+-- local Variables
+
+CompanyImage = display.newImage( "Images/IMG_4635 (2).png" )
+CompanyImage:scale (0.2, 0.2)
 
 -- making the Image move to the center of the screen.
 transition.to(CompanyImage, {rotation = CompanyImage.rotation - 360, time = 1000, onComplete = spinImage})
@@ -27,7 +44,12 @@ transition.to(CompanyImage, {x = 512, y = 384, time = 4000})
 CompanyImage.alpha = 0
 
 -- playing the sound
-IntroSoundChannel = audio.play(IntroSound)
+
+
+-----------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+-----------------------------------------------------------------------------------------
+
 
 local function MoveLogo(event)
 	--add the scroll speed to the x-value of the logo
@@ -36,5 +58,8 @@ local function MoveLogo(event)
 	CompanyImage.alpha = CompanyImage.alpha + 0.01
 end
 
+-----------------------------------------------------------------------------------------
+-- LISTENERS
+-----------------------------------------------------------------------------------------
 --moveShip will de called over and over again
 Runtime:addEventListener("enterFrame", MoveLogo)
